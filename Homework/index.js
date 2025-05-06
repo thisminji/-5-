3a4ab9cd344bc787ffc3f6a2fbcb8e6951ac6c59
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2025-05-06 14:38:12 +0900

    Homework 추가

---
+++ Homework/index.js
@@ -0,0 +1,9 @@
+//index.js
+customTag.changeTagName('P');
+customTag.changeTextContent('아기사자');
+customTag.changeStyles('color', 'red');
+customTag.changeStyles('fontSize', '20px');
+customTag.setId('myId');
+customTag.addClassName('myClass1');
+customTag.addClassName('myClass2');
+customTag.removeClassName('myClass2');
